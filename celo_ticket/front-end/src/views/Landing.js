ec1a3c2b8d6ce0a04dfedc03488bd6c05a269384
Naren <narenks@gmail.com>
2022-11-13 13:37:07 +0800

    front end

---
+++ celo_ticket/front-end/src/views/Landing.js
@@ -0,0 +1,113 @@
+import React from "react";
+
+import Navbar from "components/Navbar.js";
+import Footer from "components/Footer.js";
+
+export default function Landing() {
+  return (
+    <>
+      <Navbar transparent />
+      <main>
+        <div className="relative pt-16 pb-32 flex content-center items-center justify-center"
+            style={{
+              minHeight: "75vh"
+            }}>
+          <div className="absolute top-0 w-full h-full bg-center bg-cover"
+              style={{
+                backgroundImage: "url('https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80')"
+              }}>
+            <span id="blackOverlay" className="w-full h-full absolute opacity-75 bg-black"></span>
+          </div>
+          <div className="container relative mx-auto">
+              <div className="items-center flex flex-wrap">
+                <div className="w-full lg:w-6/12 px-4 ml-auto mr-auto text-center">
+                  <div className="pr-12">
+                    <h1 className="text-white font-semibold text-5xl">
+                      Celo Ticket App
+                    </h1>
+                    <p className="mt-4 text-lg text-gray-300">
+                      Engage and reward the participants with cool NFTs!
+                    </p>
+                  </div>
+                </div>
+
+              </div>
+          </div>
+          <div
+            className="top-auto bottom-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden"
+            style={{ height: "70px" }}
+          >
+            <svg
+              className="absolute bottom-0 overflow-hidden"
+              xmlns="http://www.w3.org/2000/svg"
+              preserveAspectRatio="none"
+              version="1.1"
+              viewBox="0 0 2560 100"
+              x="0"
+              y="0"
+            >
+              <polygon
+                className="text-gray-300 fill-current"
+                points="2560 0 2560 100 0 100"
+              ></polygon>
+            </svg>
+          </div>
+        </div>
+
+        <section className="pb-20 bg-gray-300 -mt-24">
+          <div className="container mx-auto px-4">
+            <div className="flex flex-wrap">
+              <div className="lg:pt-12 pt-6 w-full md:w-4/12 px-4 text-center">
+                <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
+                  <div className="px-4 py-5 flex-auto">
+                    <div className="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-red-400">
+                      <i className="fas fa-award"></i>
+                    </div>
+                    <h6 className="text-xl font-semibold">  Step 1 - Register</h6>
+                    <p className="mt-2 mb-4 text-gray-600">
+                      Register your event
+                    </p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="w-full md:w-4/12 px-4 text-center">
+                <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
+                  <div className="px-4 py-5 flex-auto">
+                    <div className="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-blue-400">
+                      <i className="fas fa-retweet"></i>
+                    </div>
+                    <h6 className="text-xl font-semibold">
+                      Step 2 - Review
+                    </h6>
+                    <p className="mt-2 mb-4 text-gray-600">
+                      We review and approve your event
+                    </p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="pt-6 w-full md:w-4/12 px-4 text-center">
+                <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
+                  <div className="px-4 py-5 flex-auto">
+                    <div className="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-green-400">
+                      <i className="fas fa-fingerprint"></i>
+                    </div>
+                    <h6 className="text-xl font-semibold">
+                      Step 3 - Mint NFTs
+                    </h6>
+                    <p className="mt-2 mb-4 text-gray-600">
+                      We mint NFTs and deliver to your participant wallet
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+         </div>
+        </section>
+
+      </main>
+      <Footer />
+    </>
+  );
+}
